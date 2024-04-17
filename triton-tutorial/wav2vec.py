20b66d930849c3a4dbfab0b67ac9366a9d7d0d93
onezach <87786580+onezach@users.noreply.github.com>
2024-04-17 14:40:34 -0500

    cleanup

--- triton-tutorial/wav2vec.py
+++ triton-tutorial/wav2vec.py
@@ -1,9 +1,5 @@
-# https://colab.research.google.com/github/PyThaiNLP/tutorials/blob/master/source/notebooks/thai_wav2vec2_onnx.ipynb
-
-import transformers
-from transformers import AutoTokenizer, Wav2Vec2ForCTC, Wav2Vec2Tokenizer
-# from torchaudio.models.wav2vec2.utils import import_huggingface_model
-
+import torch
+from transformers import Wav2Vec2ForCTC
 
 model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-base-960h")
 
@@ -11,16 +7,6 @@ sample_rate = 16000
 dummy_input = torch.randn(1, sample_rate).float()
 
 model.eval()
-# imported = import_huggingface_model(
-#     original)  # Build Wav2Vec2Model from the corresponding model object of Hugging Face https://pytorch.org/audio/stable/models.html#wav2vec2-0-hubert
-# imported.eval()  # set the model to inference mode
-
-import torch.onnx  # https://docs.microsoft.com/en-us/windows/ai/windows-ml/tutorials/pytorch-convert-model
-
-# input_size = 100000
-# AUDIO_MAXLEN = input_size
-
-# dummy_input = torch.randn(1, input_size, requires_grad=True)
 
 torch.onnx.export(model,  # model being run
                   dummy_input,  # model input (or a tuple for multiple inputs)
