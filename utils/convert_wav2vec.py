225bb5f2eb04f73593b86a31e1d0aa166aa8933c
onezach <87786580+onezach@users.noreply.github.com>
2024-04-22 13:59:36 -0500

    revert

--- utils/convert_wav2vec.py
+++ utils/convert_wav2vec.py
@@ -11,12 +11,10 @@ model.eval()
 torch.onnx.export(model,  # model being run
                   dummy_input,  # model input (or a tuple for multiple inputs)
                   "wav2vec2.onnx",  # where to save the model
-                #   export_params=True,  # store the trained parameter weights inside the model file
-                #   opset_version=14,  # the ONNX version to export the model to
-                #   do_constant_folding=True,  # whether to execute constant folding for optimization
-                  dynamic_axes={'input':[0],'output':[0]},
-                  verbose=True)
-                #   input_names=['input'],  # the model's input names
-                #   output_names=['output'],  # the model's output names
-                #   dynamic_axes={'input': {0: 'batch_size', 1: 'sequence'},  # variable length axes
-                #                 'output': {0: 'batch_size', 1: 'sequence'}})  # variable length axes
+                  export_params=True,  # store the trained parameter weights inside the model file
+                  opset_version=14,  # the ONNX version to export the model to
+                  do_constant_folding=True,  # whether to execute constant folding for optimization
+                  input_names=['input'],  # the model's input names
+                  output_names=['output'],  # the model's output names
+                  dynamic_axes={'input': {0: 'batch_size', 1: 'sequence'},  # variable length axes
+                                'output': {0: 'batch_size', 1: 'sequence'}})  # variable length axes
