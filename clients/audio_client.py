8913429803bf5124f4b0283ba0b8b44dcef6c70d
onezach <87786580+onezach@users.noreply.github.com>
2024-04-22 13:23:16 -0500

    add audio config

--- clients/audio_client.py
+++ clients/audio_client.py
@@ -8,15 +8,15 @@ sample_rate = 16000
 audio_data = np.random.randn(sample_rate).astype(np.float32)
 
 inputs = [
-    httpclient.InferInput("input.1", audio_data.shape, "FP32")
+    httpclient.InferInput("audio_input", audio_data.shape, "FP32")
 ]
 inputs[0].set_data_from_numpy(audio_data)
 
-# outputs = [
-#     httpclient.InferRequestedOutput("audio_output")
-# ] 
+outputs = [
+    httpclient.InferRequestedOutput("audio_output")
+] 
 
-results = client.infer(model_name="speech_recognition", inputs=inputs)
-output_data = results.as_numpy("308")
+results = client.infer(model_name="speech_recognition", inputs=inputs, outputs=outputs)
+output_data = results.as_numpy("audio_output")
 print("Inference output:", output_data)
 
