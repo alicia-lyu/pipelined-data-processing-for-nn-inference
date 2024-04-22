66408f37be049f65a2306b0210b7b518a37f1ff6
onezach <87786580+onezach@users.noreply.github.com>
2024-04-22 13:25:10 -0500

    bug fix

--- clients/audio_client.py
+++ clients/audio_client.py
@@ -8,15 +8,15 @@ sample_rate = 16000
 audio_data = np.random.randn(sample_rate).astype(np.float32)
 
 inputs = [
-    httpclient.InferInput("audio_input", audio_data.shape, "FP32")
+    httpclient.InferInput("input", audio_data.shape, "FP32")
 ]
 inputs[0].set_data_from_numpy(audio_data)
 
 outputs = [
-    httpclient.InferRequestedOutput("audio_output")
+    httpclient.InferRequestedOutput("output")
 ] 
 
 results = client.infer(model_name="speech_recognition", inputs=inputs, outputs=outputs)
-output_data = results.as_numpy("audio_output")
+output_data = results.as_numpy("output")
 print("Inference output:", output_data)
 
