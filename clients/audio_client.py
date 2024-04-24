1fd55fc0f966bd2c523150486884a6ac9ac4bc22
onezach <87786580+onezach@users.noreply.github.com>
2024-04-24 13:33:46 -0500

    maybe the logits were with us all along

--- clients/audio_client.py
+++ clients/audio_client.py
@@ -3,47 +3,37 @@ import numpy as np
 import soundfile as sf
 from transformers import Wav2Vec2Processor
 from datasets import load_dataset
-# import torch
+import torch
 
 processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-base-960h")
 
-# librispeech_samples_ds = load_dataset("patrickvonplaten/librispeech_asr_dummy", "clean", split="validation") # TODO: pending download from http://www.openslr.org/12
+# to be updated
+librispeech_samples_ds = load_dataset("patrickvonplaten/librispeech_asr_dummy", "clean", split="validation")
 
-# load audio
-audio_input, sample_rate = sf.read("1.wav", dtype='float32') # TODO: this audio file won't work because it is not sampled at 16kHz. Download from the link above
+# load audio - to be updated
+audio_input, sample_rate = sf.read(librispeech_samples_ds[0]["file"], dtype='float32')
 
 # pad input values and return pt tensor
 input_values = processor(audio_input, sampling_rate=sample_rate, return_tensors="pt").input_values
-print("input values:", input_values.shape)
 
-#Setup client
+# setup client
 client = httpclient.InferenceServerClient(url="localhost:8000")
 
-# audio_data, sample_rate = sf.read("1.wav", dtype='float32')
-
-audio_input = [
+inputs = [
     httpclient.InferInput("input", input_values.shape, "FP32")
 ]
-audio_input.set_data_from_numpy(input_values)
+inputs[0].set_data_from_numpy(input_values.numpy())
 
-#outputs = [
-#    httpclient.InferRequestedOutput("output")
-#] 
-
-results = client.infer(model_name="speech_recognition", inputs=[audio_input]) #, outputs=outputs)
+results = client.infer(model_name="speech_recognition", inputs=inputs)
 print(results.get_response())
 print(results.get_output("output"))
 print(results.as_numpy("output"))
-
 # retrieve logits & take argmax
 # logits = results.logits
-# predicted_ids = torch.argmax(logits, dim=-1)
+predicted_ids = torch.argmax(torch.tensor(results.as_numpy("output")), dim=-1)
 
 # transcribe
-# transcription = processor.decode(predicted_ids[0])
+transcription = processor.decode(predicted_ids[0])
 
-# print(transcription)
-
-# output_data = results.as_numpy("output")
-# print("Inference output:", output_data)
+print(transcription)
 
