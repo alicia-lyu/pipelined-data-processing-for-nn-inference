bee0eaf9320e4a1b647ddd4a84725dc3d725e2bb
alicia-lyu <alicia.w.lyu@gmail.com>
2024-04-28 17:12:26 +0000

    Reorganize client_pipeline to make the scheduler reusable

--- clients/image_pipeline.py
+++ clients/image_pipeline.py
@@ -4,81 +4,80 @@ import multiprocessing
 from multiprocessing.connection import Connection
 from image_subprocesses import batch_arrival, get_batch_args
 
-hashmap_stage = {}
-hashmap_state = {}
-CPU_USING = True
+def grant_cpu(process_id, hashmap_stage, hashmap_state): # grant CPU to a child process and update its stage and state accordingly
+    if hashmap_stage[process_id] == Stage.NOT_START:
+        hashmap_stage[process_id] = Stage.PREPROCESSING
+        hashmap_state[process_id] = CPUState.CPU
+    if hashmap_stage[process_id] == Stage.DETECTION_INFERENCE:
+        hashmap_stage[process_id] = Stage.CROPPING
+        hashmap_state[process_id] = CPUState.CPU
+    elif hashmap_stage[process_id] == Stage.RECOGNITION_INFERENCE:
+        hashmap_stage[process_id] = Stage.POSTPROCESSING
+        hashmap_state[process_id] = CPUState.CPU
 
-def schedule(min_interval, max_interval, batch_size):
-    def try_run_cpu(parent_pipe):
-        global hashmap_stage,hashmap_state,CPU_USING
+def relinquish_cpu(process_id, hashmap_stage, hashmap_state): # Update the stage and state of a child client when it relinquishes CPU
+    if hashmap_stage[process_id] == Stage.POSTPROCESSING: # Finished all stages
+        del hashmap_stage[process_id]
+        del hashmap_state[process_id]
+    elif hashmap_stage[process_id] == Stage.PREPROCESSING:
+        hashmap_stage[process_id] = Stage.DETECTION_INFERENCE
+        hashmap_state[process_id] = CPUState.GPU
+    elif hashmap_stage[process_id] == Stage.CROPPING:
+        hashmap_stage[process_id] = Stage.RECOGNITION_INFERENCE
+        hashmap_state[process_id] = CPUState.GPU
+
+def schedule(parent_pipe, grant_cpu_func, relinquish_cpu_func):
+    hashmap_stage = {}
+    hashmap_state = {}
+    cpu_using = False
+
+    def try_run_cpu():
+        nonlocal parent_pipe, hashmap_stage, hashmap_state, cpu_using
+
         try:
-            min_process_id = min(key for key, value in hashmap_state.items() if value == CPUState.WAITING_FOR_CPU)
+            min_process_id = min(key \
+                                 for key, value in hashmap_state.items() if value == CPUState.WAITING_FOR_CPU) # Allocate CPU to the eligible client with the smallest id (FIFO)
             min_process_id = int(min_process_id)
-            if hashmap_stage[min_process_id] == Stage.NOT_START:
-                hashmap_stage[min_process_id] = Stage.PREPROCESSING
-                hashmap_state[min_process_id] = CPUState.CPU
-            if hashmap_stage[min_process_id] == Stage.DETECTION_INFERENCE:
-                hashmap_stage[min_process_id] = Stage.CROPPING
-                hashmap_state[min_process_id] = CPUState.CPU
-            elif hashmap_stage[min_process_id] == Stage.RECOGNITION_INFERENCE:
-                hashmap_stage[min_process_id] = Stage.POSTPROCESSING
-                hashmap_state[min_process_id] = CPUState.CPU
+            grant_cpu_func(min_process_id, hashmap_stage, hashmap_state)
             parent_pipe.send((min_process_id, Message.CPU_AVAILABLE))
             # print("CPU working",min_process_id,str(hashmap_stage[min_process_id]))
-            CPU_USING = True
+            cpu_using = True
         except ValueError:
-            print("spare CPU, hashmap_stage:", hashmap_stage)
-            CPU_USING = False
-
+            print("No client to allocate the spare CPU. Hashmap_stage:", hashmap_stage)
+            cpu_using = False
     
-    global hashmap_stage,hashmap_state,CPU_USING
-    parent_pipe, child_pipe = Pipe()
-
-    p = multiprocessing.Process(target = batch_arrival,args=(min_interval, max_interval, batch_size, \
-            lambda batch, id : create_client(batch, id, child_pipe, parent_pipe)))
-    p.start()
-    
-
     while True:
         client_id, signal_type = parent_pipe.recv()
+        # Act on received signal from a child
         client_id = int(client_id)
-        print("pipeline received:",client_id, signal_type,CPU_USING)
+        print("pipeline received:",client_id, signal_type,cpu_using)
         print(hashmap_stage)
         print(hashmap_state)
-        if signal_type == Message.CREATE_PROCESS:
-            if client_id == 0:
-                hashmap_stage[client_id] = Stage.PREPROCESSING
-                hashmap_state[client_id] = CPUState.CPU
-            else: 
-                hashmap_stage[client_id] = Stage.NOT_START
-                hashmap_state[client_id] = CPUState.WAITING_FOR_CPU
-                if not CPU_USING:
-                    try_run_cpu(parent_pipe)
-        elif signal_type == Message.CPU_AVAILABLE:
-            # TODO: Schedule the incomplete process with the smallest id in the hashmap √
-            # TODO: Update hashmap: Add the process that is first scheduled OR update the stage of an active process √
-            if hashmap_stage[client_id] == Stage.POSTPROCESSING:
-                del hashmap_stage[client_id]
-                del hashmap_state[client_id]
-            elif hashmap_stage[client_id] == Stage.PREPROCESSING:
-                hashmap_stage[client_id] = Stage.DETECTION_INFERENCE
-                hashmap_state[client_id] = CPUState.GPU
-            elif hashmap_stage[client_id] == Stage.CROPPING:
-                hashmap_stage[client_id] = Stage.RECOGNITION_INFERENCE
-                hashmap_state[client_id] = CPUState.GPU
-            try_run_cpu(parent_pipe)
-        elif signal_type == Message.WAITING_FOR_CPU:
+        if signal_type == Message.CREATE_PROCESS: # A child client is first created (in create_client)
+            hashmap_stage[client_id] = Stage.NOT_START
             hashmap_state[client_id] = CPUState.WAITING_FOR_CPU
-            if not CPU_USING:
-                try_run_cpu(parent_pipe)
+            if not cpu_using:
+                try_run_cpu()
 
-def create_client(image_paths, process_id, child_pipe, parent_pipe):
+        elif signal_type == Message.CPU_AVAILABLE: # A child client relinquishes CPU
+            relinquish_cpu_func(client_id, hashmap_stage, hashmap_state)
+            try_run_cpu() # Allocate the available CPU according to the policy coded in try_run_cpu
+
+        elif signal_type == Message.WAITING_FOR_CPU: # A child client finishes GPU tasks and is now waiting for CPU
+            hashmap_state[client_id] = CPUState.WAITING_FOR_CPU
+            if not cpu_using: # When the CPU is not allocated when it first became available
+                try_run_cpu()
+
+def create_client(image_paths, process_id, child_pipe):
     child_pipe.send((process_id, Message.CREATE_PROCESS))
     p = multiprocessing.Process(target=client, args=(image_paths,process_id,child_pipe))
     p.start()
-    if process_id == 0: # start the first process upon creation
-        parent_pipe.send((0, Message.CPU_AVAILABLE))
 
 if __name__ == "__main__":
     args = get_batch_args()
-    schedule(args.min, args.max, args.batch_size)
+    parent_pipe, child_pipe = Pipe()
+    # Non-blocking (run at the same time with the scheduler): images arrive in batch
+    p = multiprocessing.Process(target = batch_arrival, args=(args.min, args.max, args.batch_size, \
+            lambda batch, id : create_client(batch, id, child_pipe)))
+    p.start()
+    schedule(parent_pipe, grant_cpu, relinquish_cpu)
