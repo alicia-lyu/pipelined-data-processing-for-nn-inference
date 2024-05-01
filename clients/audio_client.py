0b2521df3a85e71cb34e52174f2e0718977ddde3
onezach <87786580+onezach@users.noreply.github.com>
2024-05-01 13:38:43 -0500

    walkback

--- clients/audio_client.py
+++ clients/audio_client.py
@@ -7,6 +7,7 @@ import sys
 from multiprocessing.connection import Connection
 from enum import Enum
 import time
+from datasets import load_dataset
 
 class Message(Enum):
     CPU_AVAILABLE = "CPU_AVAILABLE"
@@ -57,11 +58,18 @@ def main(audio_paths, process_id, signal_pipe: Connection = None):
     print(f"t1: {t1}")
     processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-base-960h")
 
+    librispeech_samples_ds = load_dataset("patrickvonplaten/librispeech_asr_dummy", "clean", split="validation")
+    # load audio - to be updated
+    # audio_input, sample_rate = sf.read(librispeech_samples_ds[0]["file"], dtype='float32')
+    # pad input values and return pt tensor
+    # preprocessed_audio = processor(audio_input, sampling_rate=sample_rate, return_tensors="pt").input_values
+
+
     # preprocessed_audios = []
     # for path in audio_paths:
     #     preprocessed_audios.append(audio_preprocess(path, processor))
 
-    preprocessed_audio = audio_preprocess(audio_paths[0], processor)
+    preprocessed_audio = audio_preprocess(librispeech_samples_ds[0]["file"], processor)
 
     t2 = time.time()
     print(f"t2: {t2}")
